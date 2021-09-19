22b8e860c4b02e2ac9a8aa4fde82becda02f8d20
Margus <margusk@gmail.com>
2021-09-19 20:46:46 +0000

    Code reformat.

--- src/GetSetTrait.php
+++ src/GetSetTrait.php
@@ -6,14 +6,77 @@ namespace margusk\GetSet;
 
 use BadMethodCallException;
 use InvalidArgumentException;
-use ReflectionClass;
-use ReflectionProperty;
+use margusk\GetSet\Attributes\Delete;
 use margusk\GetSet\Attributes\Get;
 use margusk\GetSet\Attributes\Set;
-use margusk\GetSet\Attributes\Delete;
+use ReflectionClass;
+use ReflectionProperty;
 
 trait GetSetTrait
 {
+    public function __call(string $method, array $args): mixed
+    {
+        $conf = $this->loadGetSetConfiguration();
+
+        $lcaseMethod = strtolower($method);
+        $prefix = substr($lcaseMethod, 0, 3);
+
+        // Try to detect "setProperty", "getProperty", "issetProperty" or "unserProperty" calls.
+        if ('set' !== $prefix
+            && 'get' !== $prefix
+            && !in_array(($prefix = substr($lcaseMethod, 0, 5)), ['unset', 'isset'])
+        ) {
+            $prefix = null;
+        }
+
+        $property = substr($method, strlen((string)$prefix));
+        $lcaseProperty = strtolower($property);
+
+        // If not one of the explicit calls above, then check if whole method name is property name like
+        //  $obj->somePropertyName('somevalue')
+        if (null === $prefix && isset($conf['byLCase'][$lcaseProperty])) {
+            // If there is zero arguments, then interpret the call as Getter
+            // If there are arguments, then it's Setter
+            if (count($args) > 0) {
+                $prefix = 's';
+            } else {
+                $prefix = 'g';
+            }
+        }
+
+        if (null !== $prefix) {
+            $property = $conf['byLCase'][strtolower($property)] ?? $property;
+
+            // Call Setter
+            if ('s' === $prefix[0]) {
+                if (1 !== count($args)) {
+                    throw new BadMethodCallException('expecting 1 argument to method %s', $method);
+                }
+                $this->__set($property, $args[0]);
+                return $this;
+                // Getter, Setter, Isset
+            } else {
+                if (0 !== count($args)) {
+                    throw new BadMethodCallException('no arguments expected to method %s', $method);
+                }
+
+                // Call Getter
+                if ('g' === $prefix[0]) {
+                    return $this->__get($property);
+                    // Call Isset
+                } elseif ('i' === $prefix[0]) {
+                    return $this->__isset($property);
+                }
+
+                // Call Unsetter
+                $this->__unset($property);
+                return $this;
+            }
+        }
+
+        throw new BadMethodCallException(sprintf('unknown method %s', $method));
+    }
+
     protected function loadGetSetConfiguration(): array
     {
         static $conf = [];
@@ -22,12 +85,11 @@ trait GetSetTrait
 
         if (!isset($conf[$cl])) {
             $conf[$cl] = [
-                'byCase' => [],
+                'byCase'  => [],
                 'byLCase' => []
             ];
 
-            $parseAttributes = function (ReflectionClass|ReflectionProperty $reflection, ?array $conf) : array
-            {
+            $parseAttributes = function (ReflectionClass|ReflectionProperty $reflection, ?array $conf): array {
                 if (null === $conf) {
                     $conf = [
                         'get'     => false,
@@ -70,9 +132,11 @@ trait GetSetTrait
             $classConf = $parseAttributes($reflectionClass, null);
 
             // Parse attributes of each property
-            foreach ($reflectionClass->getProperties(
-                ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE
-            ) as $reflectionProperty) {
+            foreach (
+                $reflectionClass->getProperties(
+                    ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE
+                ) as $reflectionProperty
+            ) {
                 $property = $reflectionProperty->getName();
                 $lcaseProperty = strtolower($property);
 
@@ -103,6 +167,23 @@ trait GetSetTrait
         return $conf[$cl];
     }
 
+    public function __get(string $property): mixed
+    {
+        $conf = $this->loadGetSetConfiguration();
+
+        if (!isset($conf['byCase'][$property])) {
+            throw new InvalidArgumentException(sprintf('tried to read unknown property "%s"', $property));
+        }
+
+        $conf = $conf['byCase'][$property];
+
+        if (!$conf['get']) {
+            throw new InvalidArgumentException(sprintf('tried to read private/protected property "%s"', $property));
+        }
+
+        return $this->{$property};
+    }
+
     public function __set(string $property, mixed $value): void
     {
         $conf = $this->loadGetSetConfiguration();
@@ -122,23 +203,6 @@ trait GetSetTrait
         }
 
         $this->{$property} = $value;
-    }
-
-    public function __get(string $property): mixed
-    {
-        $conf = $this->loadGetSetConfiguration();
-
-        if (!isset($conf['byCase'][$property])) {
-            throw new InvalidArgumentException(sprintf('tried to read unknown property "%s"', $property));
-        }
-
-        $conf = $conf['byCase'][$property];
-
-        if (!$conf['get']) {
-            throw new InvalidArgumentException(sprintf('tried to read private/protected property "%s"', $property));
-        }
-
-        return $this->{$property};
     }
 
     public function __isset(string $property): bool
@@ -173,68 +237,5 @@ trait GetSetTrait
         }
 
         unset($this->{$property});
-    }
-
-    public function __call(string $method, array $args): mixed
-    {
-        $conf = $this->loadGetSetConfiguration();
-
-        $lcaseMethod = strtolower($method);
-        $prefix = substr($lcaseMethod, 0, 3);
-
-        // Try to detect "setProperty", "getProperty", "issetProperty" or "unserProperty" calls.
-        if ('set' !== $prefix &&
-            'get' !== $prefix &&
-            !in_array(($prefix = substr($lcaseMethod, 0, 5)), ['unset', 'isset'])
-        ) {
-            $prefix = null;
-        }
-
-        $property = substr($method, strlen((string)$prefix));
-        $lcaseProperty = strtolower($property);
-
-        // If not one of the explicit calls above, then check if whole method name is property name like
-        //  $obj->somePropertyName('somevalue')
-        if (null === $prefix && isset($conf['byLCase'][$lcaseProperty])) {
-            // If there is zero arguments, then interpret the call as Getter
-            // If there are arguments, then it's Setter
-            if (count($args) > 0) {
-                $prefix = 's';
-            } else {
-                $prefix = 'g';
-            }
-        }
-
-        if (null !== $prefix) {
-            $property = $conf['byLCase'][strtolower($property)] ?? $property;
-
-            // Call Setter
-            if ('s' === $prefix[0]) {
-                if (1 !== count($args)) {
-                    throw new BadMethodCallException('expecting 1 argument to method %s', $method);
-                }
-                $this->__set($property, $args[0]);
-                return $this;
-            // Getter, Setter, Isset
-            } else {
-                if (0 !== count($args)) {
-                    throw new BadMethodCallException('no arguments expected to method %s', $method);
-                }
-
-                // Call Getter
-                if ('g' === $prefix[0]) {
-                    return $this->__get($property);
-                // Call Isset
-                } elseif ('i' === $prefix[0]) {
-                    return $this->__isset($property);
-                }
-
-                // Call Unsetter
-                $this->__unset($property);
-                return $this;
-            }
-        }
-
-        throw new BadMethodCallException(sprintf('unknown method %s', $method));
     }
 }
