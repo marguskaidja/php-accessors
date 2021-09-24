9e635d24fac89917b3beabcc5e3b00059802cc0d
Margus <margusk@gmail.com>
2021-09-24 09:07:07 +0000

    Missed attribute file.

---
+++ src/Attributes/CaseInsensitive.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the GetSet package.
+ *
+ * @author  Margus Kaidja <margusk@gmail.com>
+ * @link    https://github.com/m4r9u5/GetSet
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ */
+
+declare(strict_types=1);
+
+namespace margusk\GetSet\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS]
+class CaseInsensitive
+{
+
+}
