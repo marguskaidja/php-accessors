766f6885dabd7560c5258fb279fd3ef3a8158958
Margus Kaidja <margusk@gmail.com>
2021-10-17 18:47:13 +0000

    Update github link in copyright information. Convert CRLF -> LF.

--- src/Exceptions/BadMethodCallException.php
+++ src/Exceptions/BadMethodCallException.php
@@ -4,7 +4,7 @@
  * This file is part of the GetSet package.
  *
  * @author  Margus Kaidja <margusk@gmail.com>
- * @link    https://github.com/m4r9u5/GetSet
+ * @link    https://github.com/marguskaidja/php-getset
  * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
  */
 
