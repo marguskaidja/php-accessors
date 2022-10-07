4a71ab3fec0871de118a6fd5c6bb399ceb5ada99
Margus Kaidja <margusk@gmail.com>
2022-10-07 08:31:56 +0000

    Add copyrights.

--- src/Core.php
+++ src/Core.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the GetSet package.
+ *
+ * @author  Margus Kaidja <margusk@gmail.com>
+ * @link    https://github.com/marguskaidja/php-getset
+ * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ */
+
 declare(strict_types=1);
 
 namespace margusk\GetSet;
